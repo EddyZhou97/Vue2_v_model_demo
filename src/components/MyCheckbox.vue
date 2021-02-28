d1e1adedc11307ffdcbba270f1e19ac5adf28ee8
EddyZhou97 <36226819+EddyZhou97@users.noreply.github.com>
2021-02-28 21:32:56 +0800

    添加注释

--- src/components/MyCheckbox.vue
+++ src/components/MyCheckbox.vue
@@ -8,8 +8,11 @@ export default {
   props: {
     checked: Boolean,
   },
+  // 通过定义model中的 prop 和 event 来重载默认的 value 属性和 input 事件
   model: {
+    // 绑定的属性
     prop: "checked",
+    // 触发的事件
     event: "change",
   },
   methods: {
